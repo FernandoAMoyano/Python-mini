0fd5119f15d3d2074fff1473acae4587749dccdd
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-04-20 14:45:21 -0300

    Ejercicio22

--- ejercicio22.py
+++ ejercicio22.py
@@ -1,7 +1,49 @@
 """
-Escribe un programa que administre el inventario de una 
-tienda , el programa debe permitir agregar nuevos productos
-actualizar las cantidades de los productos existentes y
-mostrar el inventario actual
+Escribe un programa que administre el inventario de una
+tienda , el programa debe permitir:
+1_ agregar nuevos productos
+2_ actualizar las cantidades de los productos existentes
+3_ mostrar el inventario actual
 
 """
+
+lista_productos = [
+    {"id": 1, "nombre": "Mouse", "precio": 25, "cantidad" "stock": 30},
+    {"id": 2, "nombre": "Teclado", "precio": 45, "stock": 20},
+]
+
+producto1 = {"nombre": "Laptop", "precio": 1200, "stock": 5}
+
+
+def agrearProducto(nuevoProducto):
+    lista_productos.append(nuevoProducto)
+
+
+def actualizarProducto(id, nuevasCaracteristicas):
+    for i in range(len(lista_productos)):
+        if lista_productos[i]["id"] == id:
+            if "nombre" in nuevasCaracteristicas:
+                lista_productos[i]["nombre"] = nuevasCaracteristicas["nombre"]
+            if "precio" in nuevasCaracteristicas:
+                lista_productos[i]["precio"] = nuevasCaracteristicas["precio"]
+            if "cantidad" in nuevasCaracteristicas:
+                lista_productos[i]["cantidad"] = nuevasCaracteristicas["cantidad"]
+            if "stock" in nuevasCaracteristicas:
+                lista_productos[i]["stock"] = nuevasCaracteristicas["stock"]
+            print(f"Producto con ID {id} actualizado parcialmente.")
+            return
+        print(f"No se encontro el producto con ID{id}")
+
+
+def mostrarProductos():
+    mensaje = "Productos en el carrito: \n"
+    for producto in lista_productos:
+        mensaje += f"{producto["nombre"]} ${producto["precio"]}\n"
+    print(mensaje)
+
+
+mostrarProductos()
+actualizarProducto(1, {"nombre": "Leche", "precio": 43, "stock": 54})
+mostrarProductos()
+agrearProducto(producto1)
+mostrarProductos()
