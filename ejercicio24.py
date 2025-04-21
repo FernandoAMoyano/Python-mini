b9505f158610cfb644aafa95b1f9cc3fc04f7a29
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-04-21 12:05:38 -0300

    ejercicio 24

--- ejercicio24.py
+++ ejercicio24.py
@@ -3,3 +3,10 @@ Escribe una funcion que reciba como parametros un nombre
 y devuelva un saludo personalizado
 
 """
+
+
+def saludar(nombre):
+    return print(f"Hola {nombre}!! bienvenido a la aplicacion")
+
+
+saludar("Fernando")
