659362faedf0712e47d10ae9153190a4cdb01ae5
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-04-23 12:38:18 -0300

    Parte2 ejercicio1 y ejercicio2

--- Parte2/ejercicio1.py
+++ Parte2/ejercicio1.py
@@ -1,6 +1,21 @@
+"""
+Escriba un programa que pida al usuario un entero de
+tres digitos y entregue el numero con los digitos en
+orden inverso.
 
 """
-Escriba un programa que pida al usuario un entero de tres digitos
-y entregue el numero con los digitos en orden inverso
 
-"""
+while True:
+    numero = input("Ingresa un numero de 3 digitos")
+    try:
+        if numero.isdigit():
+            if len(numero) == 3:
+                a = numero[0]
+                b = numero[1]
+                c = numero[2]
+                print("Numero invertido: " + c + b + a)
+                break
+            print("El numero debe tener 3 digitos")
+        print("El valor ingresado debe ser un numero")
+    except:
+        print("El numero no es valido")
--- Parte2/ejercicio2.py
+++ Parte2/ejercicio2.py
@@ -1,9 +1,29 @@
-
-""" 
+"""
 
 Escriba un programa que pregunte al usuario la hora
-actual t del reloj y un numero entero de 
+actual t del reloj y un numero entero de
 horas h, que indique que hora marcara el reloj
 dentro de h horas
 
 """
+
+hora_actual = int(input("Ingresa la hora actual"))
+cantidad_horas = int(input("Ingresa una cantidad de horas"))
+
+hora_futura = (hora_actual + cantidad_horas)%24
+
+
+if hora_futura == 0:
+    hora_a_mostrar = 12
+    periodo = "AM"
+elif hora_futura < 12:
+    hora_a_mostrar = hora_futura
+    periodo = "AM"
+elif hora_futura == 12:
+    hora_a_mostrar = 12
+    periodo = "PM"
+else:
+    hora_a_mostrar = hora_futura -12
+    periodo = "PM"
+    
+print(f"La hora dentro de {cantidad_horas} horas será: {hora_a_mostrar} {periodo}")
