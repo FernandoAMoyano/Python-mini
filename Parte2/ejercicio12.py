988d2a798aa509540ff76165c3ae28f7d2fc3193
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-05-03 20:18:53 -0300

    Parte2 enunciado ejercicio 12

---
+++ Parte2/ejercicio12.py
@@ -0,0 +1,15 @@
+""" 
+Torneo de Tenis
+
+Escriba un programa para simular un campeonato de tenis.
+
+- Primero, debe pedir al usuario que ingrese los
+nombres de ocho tenistas.
+- A continuacion debe pedir los resultados de los partidos
+juntando los jugadores de dos en dos.El ganador de cada partido
+avanza a la ronda siguiente.
+
+- El programa debe continuar preguntando ganadores
+de partidos hasta que quede un unico jugador, que es
+el campeon del torneo.
+"""
