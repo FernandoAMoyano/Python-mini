4d98310fd2d8738128f2cbfde69ac545ac422c76
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-05-03 20:41:03 -0300

    completando enunciando 16

---
+++ Parte2/ejercicio16.py
@@ -0,0 +1,14 @@
+""" 
+Código Morse
+
+Crea un programa que se capaz de transformar texto natural a
+codigo morse y viceversa.
+
+
+-Debe detectar automaticamente de que tipo se trata y realizar la conversion
+- En morse se soporta raya "-", punto "." un espacio " " entre letras o simbolos
+y dos espacios entre palabras " ".
+- El alfabeto morse soportado setá el mostrado en 
+
+https://es.wikipedia.org/wiki/C%C3%B3digo_morse
+"""
