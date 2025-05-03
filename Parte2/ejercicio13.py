2573f60af1e264735f48a2d249223ff82a33d6ca
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-05-03 20:22:26 -0300

    Parte2 enunciado ejercicio 13

---
+++ Parte2/ejercicio13.py
@@ -0,0 +1,14 @@
+""" 
+El diccionario paises asocia cada persona con 
+el conjunto de los paises que ha visitado
+
+paises={
+  
+  "Pepito":{"Chile", "Argentina"}
+  "Yayita":{"Francia", "Suiza", "Chile"}
+  "Jhon":{"Chile", "Italia", "Francia", "Peru"}
+}
+
+escriba una funcion cuantos_en_comun(a,b) que indique cuantos paises
+en comun han visitado la persona a y la persona b
+"""
