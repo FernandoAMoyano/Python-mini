88240710d02f11fb103bf9fffeb56719f88363ee
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-05-03 20:14:14 -0300

    Parte2 Enunciado ejercicio 11

---
+++ Parte2/ejercicio11.py
@@ -0,0 +1,17 @@
+""" 
+Piedra, papel y tijera
+
+En cada ronda del juego del cachipun los dos competidores
+deben elegit entre jugar tijera, papel, o piedra.
+
+Las reglas para decidir quien gana laronda son: tijera le gana a papel
+papel le gana a piedra piedra le gana a tijera y todas las demas
+combinaciones son empates.
+
+El ganador del juego es el primero que gane tres rondas.
+
+Escriba un programa que pregunte a cada jugador cual es su jugada, muestre
+cual es el marcador despues de cada ronda y termine cuando uno de ellos haya ganado tres
+rondas.Los jugadores deben indicar su jugada escribiendo tijera papel o piedra.
+
+"""
