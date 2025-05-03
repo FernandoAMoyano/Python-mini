d157f204d4f32c3a9ff4be4c856f8ecfcb7785ca
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-05-03 20:10:12 -0300

    Parte2 enunciado 10

---
+++ Parte2/ejercicio10.py
@@ -0,0 +1,12 @@
+""" 
+Torre y Alfil
+
+Un tablero de ajederez es una grilla de ocho filas y ocho columnas,
+numeradas de 1 a 8 Dos de las piezas del juego de ajedrez son el alfil
+y la torre, el alfil se desplaza en diagonal mientras que la torre
+se desplaza horizontal o verticalmente.Una pieza pude ser capturada por 
+otra si está en una casilla a la cual la otra puede desplazarse.
+
+Escriba un programa que reciba como entrada las posiciones en el tablero
+de un alfil y de una torre, e indique cual pieza captura a la otra.
+"""
