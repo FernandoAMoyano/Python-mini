fd93531f9dcc4273ff7682e258b205bf8efe3840
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-05-03 20:30:01 -0300

    Parte2 enunciado ejercicio 14

---
+++ Parte2/ejercicio14.py
@@ -0,0 +1,27 @@
+""" 
+Signo Zodiacal
+
+El signo zodiacal de una persona está determinado por su dia
+de nacimiento.
+
+El diccionario de signos asocia a cada signo el periodo del año
+que le corresponde. Cada periodo es una tupla con la fecha de inicio
+y la fecha de termino, y cada fecha es una tupla(mes, dia)
+
+signos = {
+  
+  "aries": ((3,21),(4,20))
+  "tauro": ((4,21),(5,21))
+  "geminis": ((5,22),(6,21))
+  "cancer": ((6,22),(7,23))
+  "leo": ((7,24),(8,23))
+  "virgo": ((8,24),(9,23))
+  "libra": ((9,24),(10,23))
+  "escorpio": ((10,24),(11,22))
+  "sagitario": ((11,23),(12,21))
+  "capricornio": ((12,22),(1,20))
+  "acuario": ((1,21),(2,19))
+  
+}
+
+"""
