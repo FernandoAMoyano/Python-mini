82d96b6ca8b55037ae47e8e567146056850d7888
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-08-07 18:14:32 -0300

    herencia jerarquica

---
+++ Parte4-POO/herencia_jerarquica/herencia_jerarquica.py
@@ -0,0 +1,27 @@
+class Robot:
+  def __init__(self, nombre):
+    self.nombre = nombre
+
+def saludar(self):
+  return f"{self.nombre} dice hola."
+
+
+class RobotExplorador(Robot):
+  def explorar(self):
+    return f"{self.nombre} está explorando el terreno."
+  
+  
+class RobotCocinero(Robot):
+  def cocinar(self):
+    return f"{self.nombre} está preparando una deliciosa comida."
+
+
+# Ejemplo de uso
+robot1 = RobotExplorador(nombre="Rover")
+robot2 = RobotCocinero(nombre="ChefBot")
+# Usar los métodos heredados
+print(robot1.saludar()) # Saludo del RobotExplorador
+print(robot2.saludar()) # Saludo del RobotCocinero
+# Usar métodos específicos de cada clase
+print(robot1.explorar())
+print(robot2.cocinar())
