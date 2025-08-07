4a2ff975404cf547675ebe91c8cb0c9345686548
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-08-07 16:31:40 -0300

    plantilla clase Personaje

---
+++ Parte4-POO/classPersonaje/personaje.py
@@ -0,0 +1,27 @@
+class Personaje:
+    def __init__(self, nombre, vidas):
+        self.nombre = nombre
+        self.vidas = vidas
+
+    def __add__(self, cantidad):
+        """Aumenta las vidas del personaje en una cantidad
+        específica."""
+        self.vidas += cantidad
+        return Personaje(self.nombre, self.vidas)
+
+    def __sub__(self, cantidad):
+        """Disminuye las vidas del personaje en una cantidad
+        específica."""
+        self.vidas -= cantidad
+        return Personaje(self.nombre, self.vidas)
+
+
+if __name__ == "__main__":
+    # Crear un héroe
+    heroe = Personaje("Valiente", 10)
+    # Aumentar las vidas y el ataque del héroe
+    heroe_mejorado = heroe + 10
+    print(f"Héroe mejorado: {heroe_mejorado.nombre} (Vidas: {heroe_mejorado.vidas})")
+    # Disminuir las vidas y el ataque del héroe
+    heroe_debil = heroe - 2
+    print(f"Héroe debilitado: {heroe_debil.nombre} (Vidas: {heroe_debil.vidas})")
